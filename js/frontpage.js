db1b1a702ad492312b94b0cc0ba1faa73f9cb7f0
death667b <idaniel@spinnys.com>
2017-07-16 10:59:22 +1000

    new stuff

---
+++ js/frontpage.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){	
+	$('#menu').slicknav();
+
+	$( ".login-submit-btn" ).click(function() {
+		window.location.href = 'dashboard.html';
+	});
+});
--- js/main.js
+++ js/main.js
@@ -21,4 +21,83 @@ $(document).ready(function(){
 	});
 	
 	$('#menu').slicknav();
+
+	$('.sign-me-out').click(function() {
+		window.location.href = 'index.html';
+	});
+
+	calcThenDisplay();
+
+	$( ".update-trips-btn" ).click(addRandomTrip);
+	$( ".trip-calc-btn" ).click(calcEstCost);
 });
+
+const calcEstCost = () => {
+	let estCostPeak = 0;
+	let estCostOffPeak = 0;
+	let showError = false;
+
+	let valueOut = $('#numOutCBD').val();
+	if (isNaN(valueOut)) { 
+		valueOut = 0; 
+		showError = true;
+	} 
+
+	estCostPeak = parseFloat(valueOut) * 0.1; 
+	estCostOffPeak = parseFloat(valueOut) * 0.04; 
+
+	let valueIn = $('#numInCBD').val();
+	if (isNaN(valueIn)) {
+		valueIn = 0;
+		showError = true;
+	}
+
+	if (valueIn > 0) {
+		estCostPeak += (parseFloat(valueIn * 0.1)) + 5; 
+		estCostOffPeak += (parseFloat(valueIn * 0.04)) + 5;
+	}
+
+	$('.est-cost-peak-display').text('$' + estCostPeak.toFixed(2));
+	$('.est-cost-offpeak-display').text('$' + estCostOffPeak.toFixed(2));
+
+	if (showError) {
+		$('.calc-error').css("visibility", "visible");
+	} else {
+		$('.calc-error').css("visibility", "hidden");
+	}
+	
+}
+
+const calcThenDisplay = () => {
+	let tripTotal = 0;
+	$( ".trip-total" ).each(function() {
+		tripTotal += parseFloat($( this ).text().slice(1));
+	});
+	$( ".current-owing" ).text("$"+tripTotal.toFixed(2));
+}
+
+const addRandomTrip = () => {
+	let numberOfElements = $('.tablist-number').length;
+	$('.super-tab-list').prepend(`
+	<li><a href="#abc${numberOfElements}" aria-controls="abc${numberOfElements}" role="tab" data-toggle="tab">
+									<i class="fa fa-circle"></i>
+									xxxx <span class='tablist-number'>0</span>
+									</a></li>
+	`);
+
+
+
+
+	incTabListNumber();
+}
+
+const incTabListNumber = () => {
+	$( ".tablist-number" ).each(function(idx) {
+		let newNum = parseInt($( this ).text())+1;
+		$(this).text(newNum);
+
+		if (idx >= 5) {
+			$(this).parent().hide();
+		}
+    });
+}
